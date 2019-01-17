29f929c9bc7fea6651b8c17dad53968cf47215ab
Brent  Ransom <sadumedusa@gmail.com>
2019-01-17 04:40:23 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,11 @@
 # Create your costume_stores migration here
+class CreateCostumeStore < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.
+      t.timestamp
+end
