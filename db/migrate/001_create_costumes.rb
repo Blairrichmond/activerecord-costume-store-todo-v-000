5ab6ba9f1ba202a6f92b75c8be2757b72bd8515e
Brent  Ransom <sadumedusa@gmail.com>
2019-01-17 04:35:44 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,5 +10,9 @@
 
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
-    create_table :costumes do
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.integer :size 
+      t.string 
 end
