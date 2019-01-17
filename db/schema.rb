f323623c3fe21c916c98e7c6c0348c246d08baec
Brent  Ransom <sadumedusa@gmail.com>
2019-01-17 04:54:53 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,28 +16,30 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.string "location"
     t.integer "costume_inventory"
-    t.integer "number_of_employees"
+    t.string "num_of_employees"
     t.boolean "still_in_business"
-    t.integer "opening_time"
-    t.integer "closing_time"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
   end
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.string "price"
     t.string "size"
     t.string "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
     t.string "location"
     t.string "theme"
-    t.integer "price"
+    t.string "price"
     t.boolean "family_friendly"
-    t.integer "opening_date"
-    t.integer "closing_date"
-    t.string "long_description"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
+    t.string "description"
   end
 
 end
